2aae7930a1f8cbff2b3f90b415c75484020b611e
bradbread <48189304+bradbread@users.noreply.github.com>
2020-05-01 02:03:42 +1000

    Partially done on part 3
    make sure to clean up the constructor later dirty method used to get it working quickly with the data so i could test methods faster

--- FileParser/PersonHandler.cs
+++ FileParser/PersonHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using ObjectLibrary;
 
@@ -17,7 +19,11 @@ namespace FileParser {
         /// </summary>
         /// <param name="people"></param>
         public PersonHandler(List<List<string>> people) {
-
+            People = new List<Person>();
+            for (int i = 1;i < people.Count; i++)
+            {
+                People.Add(new Person(int.Parse(people[i][0]), people[i][1], people[i][2], new DateTime(Int64.Parse(people[i][3]))));
+            }
         }
 
         /// <summary>
@@ -25,8 +31,13 @@ namespace FileParser {
         /// </summary>
         /// <returns></returns>
         public List<Person> GetOldest() {
-
-            return new List<Person>(); //-- return result here
+            List<Person> oldPeople = People;
+            int length = oldPeople.Count;
+            for (int i = 0 ; i > 0; i++)
+            {
+               
+            }
+            return oldPeople; //-- return result here
         }
 
         /// <summary>
@@ -40,7 +51,26 @@ namespace FileParser {
         }
 
         public List<Person> GetOrderBySurname() {
-            return new List<Person>();  //-- return result here
+            List<Person> surnameOrder = People;
+            int length = surnameOrder.Count;
+
+            //surnameOrder.Sort();
+
+            for (int gap = length / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < length; i++)
+                {
+                    Person comp = surnameOrder[i];
+                    int d;
+                    for (d = i; d >= gap && comp.Surname.CompareTo(surnameOrder[d - gap].Surname) < 0; d -= gap)
+                    {
+                        surnameOrder[d] = surnameOrder[d - gap];
+                    }
+                    surnameOrder[d] = comp;
+                }
+            }
+
+            return surnameOrder;  //-- return result here
         }
 
         /// <summary>
@@ -50,7 +80,29 @@ namespace FileParser {
         /// <param name="caseSensitive"></param>
         /// <returns></returns>
         public int GetNumSurnameBegins(string searchTerm, bool caseSensitive) {
-            return 0;  //-- return result here
+            int result = 0;
+            if (caseSensitive)
+            {
+                for (int i = 0; i < People.Count; i++)
+                {
+                    if (People[i].Surname.StartsWith(searchTerm))
+                    {
+                        result += 1;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < People.Count; i++)
+                {
+                    if (People[i].Surname.StartsWith(searchTerm, StringComparison.OrdinalIgnoreCase))
+                    {
+                        result += 1;
+                    }
+                }
+            }
+
+            return result;  //-- return result here
         }
         
         /// <summary>
@@ -59,6 +111,11 @@ namespace FileParser {
         /// <returns></returns>
         public List<string> GetAmountBornOnEachDate() {
             List<string> result = new List<string>();
+            int length = People.Count;
+            for (int i = 0; i < length; i++)
+            {
+                result.Add(People[i].Dob.ToString("dd/mm/yyyy", CultureInfo.InvariantCulture));
+            }
 
             return result;  //-- return result here
         }
--- Tests/PersonHandlerTests.cs
+++ Tests/PersonHandlerTests.cs
@@ -13,7 +13,7 @@ namespace PersonHandlerTests
         private FileHandler _fh;
         private DataParser _dp;
 
-        private string csvPath = Environment.GetEnvironmentVariable("HOME") + "/TempFiles/processed_data.csv"; // change to appropriate path
+        private string csvPath = Environment.GetEnvironmentVariable("HOME") + "/Users/Bradley/Desktop/swinburne/diplo/ICT418/Delegates_Task/processed_data.csv"; // change to appropriate path
         private List<List<string>> data;
 
         public PersonHandlerTests() {
