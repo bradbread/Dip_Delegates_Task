5f1f6df44138d10ad528d06088bae7c02a59e34e
bradbread <48189304+bradbread@users.noreply.github.com>
2020-05-03 18:07:09 +1000

    All unit tests passed

--- FileParser/PersonHandler.cs
+++ FileParser/PersonHandler.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Globalization;
 using System.Linq;
 using ObjectLibrary;
+using System.Collections;
 
 
 namespace FileParser {
@@ -20,9 +21,25 @@ namespace FileParser {
         /// <param name="people"></param>
         public PersonHandler(List<List<string>> people) {
             People = new List<Person>();
-            for (int i = 1;i < people.Count; i++)
+
+            for (int i = 0;i < people.Count; i++)
             {
-                People.Add(new Person(int.Parse(people[i][0]), people[i][1], people[i][2], new DateTime(Int64.Parse(people[i][3]))));
+                int id;
+                long bDate;
+                bool iDSuccess = int.TryParse(people[i][0], out id);
+                bool bDateSuccess = Int64.TryParse(people[i][3], out bDate);
+                //if both the id and date are useable add the person
+                if (iDSuccess && bDateSuccess)
+                {
+                    People.Add(new Person(id, people[i][1], people[i][2], new DateTime(bDate)));
+                }
+                //otherwise handle it
+                else
+                {
+                    //not adding the person atm but should probably log/display the error in some capacity
+                    //Log.("Error in people input string oucured at line " + i + " id " + id + " birthdate " + bDate)
+                    //or something or other depending on how errors are handeld within the program
+                }
             }
         }
 
@@ -33,11 +50,30 @@ namespace FileParser {
         public List<Person> GetOldest() {
             List<Person> oldPeople = People;
             int length = oldPeople.Count;
-            for (int i = 0 ; i > 0; i++)
+            //order list
+            for (int gap = length / 2; gap > 0; gap /= 2)
             {
-               
+                for (int i = gap; i < length; i++)
+                {
+                    Person comp = oldPeople[i];
+                    int d;
+                    for (d = i; d >= gap && comp.Dob < oldPeople[d - gap].Dob; d -= gap)
+                    {
+                        oldPeople[d] = oldPeople[d - gap];
+                    }
+                    oldPeople[d] = comp;
+                }
             }
-            return oldPeople; //-- return result here
+
+            //add ordered people to the result until birth dates do not align
+            List<Person> result = new List<Person>();
+            string target = oldPeople[0].Dob.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+            for (int i = 0; oldPeople[i].Dob.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture) == target; i++)
+            {
+                result.Add(oldPeople[i]);
+            }
+
+            return result; //-- return result here
         }
 
         /// <summary>
@@ -111,10 +147,52 @@ namespace FileParser {
         /// <returns></returns>
         public List<string> GetAmountBornOnEachDate() {
             List<string> result = new List<string>();
+            List<DateTime> sortBirthday = new List<DateTime>();
+            //was going to use a hash table but it seemed a dictonary was more perfomant
+            Dictionary<string, int> dictDOB = new Dictionary<string, int>();
             int length = People.Count;
+
+            //populate a list just of birthdays
             for (int i = 0; i < length; i++)
             {
-                result.Add(People[i].Dob.ToString("dd/mm/yyyy", CultureInfo.InvariantCulture));
+                sortBirthday.Add(People[i].Dob);
+            }
+
+            //sort probably should go in it's own method in future
+            for (int gap = length / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < length; i++)
+                {
+                    DateTime comp = sortBirthday[i];
+                    int d;
+                    for (d = i; d >= gap && comp < sortBirthday[d - gap]; d -= gap)
+                    {
+                        sortBirthday[d] = sortBirthday[d - gap];
+                    }
+                    sortBirthday[d] = comp;
+                }
+            }
+
+            //turn ordered birthdates into a dictonary with the ordered dates and the number of people who had that date
+            for (int i = 0; i < length; i++)
+            {
+                //result.Add(People[i].Dob.ToString("dd/mm/yyyy", CultureInfo.InvariantCulture));
+                string s = sortBirthday[i].ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+                if (!dictDOB.ContainsKey(s))
+                {
+                    dictDOB.Add(s, 1);
+                }
+                else 
+                {
+                    dictDOB[s] = dictDOB[s] + 1;
+                }
+            }
+
+            
+            //put the dates and their counts into a list of strings to return
+            foreach (KeyValuePair<string, int> de in dictDOB)
+            {
+                result.Add(de.Key.ToString() + " " + de.Value.ToString());
             }
 
             return result;  //-- return result here
